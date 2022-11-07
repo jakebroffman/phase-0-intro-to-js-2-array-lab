91046f423996bb79139713345fd2f79654c7c166
jakebroffman <jake.broffman1088@gmail.com>
2022-11-06 19:20:49 -0500

    commit tree 1f0ff75ee30ab5f3b953874a2d7ab7190f7299e7

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop(); 
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+function appendCat(name) {
+    const newArray = [...cats, "Broom"];
+   return newArray;
+};
+
+function prependCat(name) {
+    const newArray2 = ["Arnold", ...cats];
+    return newArray2;
+};
+
+function removeLastCat(name) {
+   const cats2 = cats.slice(0, cats.length-1);
+    return cats2;
+};
+
+function removeFirstCat(name) {
+    const cats3 = cats.slice(1);
+    return cats3;
+};
